302fb2a1ec8820d615fd45a03e1207f0354d5fc2
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-07-16 18:02:37 +0530

    add useful comments for future update.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getComputerChoice() {
+    //using random number generated and dividing possibilities with 33 % each between rock paper and scissors//
     let number= Math.floor(Math.random()*100);
     
     let choice;
@@ -15,27 +16,36 @@ function getComputerChoice() {
 }
 
 function playRound(computerSelection, playerSelection) {
-
+    //comparing the playerSelection and computerSelection//
 
     if (playerSelection !== "rock" &&
         playerSelection !== "paper" && 
         playerSelection !== "scissors") {
-        message = `${playerSelection} is not a valid input, it should be rock, paper or scissors`
+
+        //the case if user inputs things that are other than rock paper or scissors//    
+        let message = `${playerSelection} is not a valid input, it should be rock, paper or scissors`;
         alert(message);
     } else if (playerSelection === computerSelection) {
-        alert("It's a tie !")
+        
+        //if the values are same//
+        alert("It's a tie !");
     } else if (
         (computerSelection === "paper" && playerSelection === "rock") ||
         (computerSelection === "rock" && playerSelection === "scissors") ||
         (computerSelection === "scissors" && playerSelection === "paper")
         ) {
-        let message = `The computer wins ! ${computerSelection} beats ${playerSelection} `
+
+        //when computer beats player//    
+        let message = `The computer wins ! ${computerSelection} beats ${playerSelection} `;
         alert(message);   
     } else if (
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper")
-    )   {let message = `You win ! ${playerSelection} beats ${computerSelection} `
+        )   {
+
+        //when user beats computer//
+        let message = `You win ! ${playerSelection} beats ${computerSelection} `
         alert(message);
         
     }
