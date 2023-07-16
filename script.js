5aa412e91194edcd2c2eb90f6c1e3ea7b198f538
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-07-16 17:46:23 +0530

    fix the loop part, add 5 times round of game.

--- script.js
+++ script.js
@@ -1,48 +1,40 @@
 function getComputerChoice() {
     let number= Math.floor(Math.random()*100);
-    console.log(number)
+    
     let choice;
     if (number < 34) {
-        choice = "Rock" ;
+        choice = "rock" ;
     }
     else if (number < 67 ) {
-        choice = "Paper" ;
+        choice = "paper" ;
     }
     else {
-        choice = "Scissors" ; 
+        choice = "scissors" ; 
     }    
     return choice;
 }
 
-
-
-
-let computerSelection = getComputerChoice();
-let rawInput = prompt("Please enter your choice");
-let playerSelection = rawInput.charAt(0).toUpperCase() + rawInput.slice(1).toLowerCase();
-
-
-console.log(computerSelection)
-
 function playRound(computerSelection, playerSelection) {
-    if (playerSelection !== "Rock" &&
-        playerSelection !== "Paper" && 
-        playerSelection !== "Scissors") {
+
+
+    if (playerSelection !== "rock" &&
+        playerSelection !== "paper" && 
+        playerSelection !== "scissors") {
         message = `${playerSelection} is not a valid input, it should be rock, paper or scissors`
         alert(message);
     } else if (playerSelection === computerSelection) {
         alert("It's a tie !")
     } else if (
-        (computerSelection === "Paper" && playerSelection === "Rock") ||
-        (computerSelection === "Rock" && playerSelection === "Scissors") ||
-        (computerSelection === "Scissors" && playerSelection === "Paper")
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "scissors" && playerSelection === "paper")
         ) {
         let message = `The computer wins ! ${computerSelection} beats ${playerSelection} `
         alert(message);   
     } else if (
-        (playerSelection === "Paper" && computerSelection === "Rock") ||
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper")
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
     )   {let message = `You win ! ${playerSelection} beats ${computerSelection} `
         alert(message);
         
@@ -52,11 +44,12 @@ function playRound(computerSelection, playerSelection) {
 
 
 function game() {
-       
-        playRound(computerSelection, playerSelection)
-        
-    
-   
+       for (let i=0; i<5; i++) {
+        let computerSelection = getComputerChoice();
+        let rawInput = prompt("Please enter your choice");
+        let playerSelection = rawInput.slice().toLowerCase();
+        playRound(computerSelection, playerSelection);
+       }
 }
 
 game();
