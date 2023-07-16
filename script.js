2bdaca63520c54b584abdde392ac8493f19ad5b4
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-07-16 16:04:46 +0530

    still missing the application of returning the 'results' in playround function, game works though.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice() {
     let number= Math.floor(Math.random()*100);
+    console.log(number)
     let choice;
     if (number < 34) {
         choice = "Rock" ;
@@ -15,32 +16,47 @@ function getComputerChoice() {
 
 
 
+
 let computerSelection = getComputerChoice();
 let rawInput = prompt("Please enter your choice");
 let playerSelection = rawInput.charAt(0).toUpperCase() + rawInput.slice(1).toLowerCase();
 
+
+console.log(computerSelection)
+
 function playRound(computerSelection, playerSelection) {
-    if (playerSelection === computerSelection) {
+    if (playerSelection !== "Rock" &&
+        playerSelection !== "Paper" && 
+        playerSelection !== "Scissors") {
+        message = `${playerSelection} is not a valid input, it should be rock, paper or scissors`
+        alert(message);
+    } else if (playerSelection === computerSelection) {
         alert("It's a tie !")
     } else if (
         (computerSelection === "Paper" && playerSelection === "Rock") ||
         (computerSelection === "Rock" && playerSelection === "Scissors") ||
         (computerSelection === "Scissors" && playerSelection === "Paper")
         ) {
-        alert("The computer wins !");   
-    } else {
-        alert("You win");
+        let message = `The computer wins ! ${computerSelection} beats ${playerSelection} `
+        alert(message);   
+    } else if (
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    )   {let message = `You win ! ${playerSelection} beats ${computerSelection} `
+        alert(message);
+        
     }
 }
 
 
 
 function game() {
-    playRound(computerSelection, playerSelection);
-    playRound(computerSelection, playerSelection);
-    playRound(computerSelection, playerSelection);
-    playRound(computerSelection, playerSelection);
-    playRound(computerSelection, playerSelection);
+       
+        playRound(computerSelection, playerSelection)
+        
+    
+   
 }
 
 game();
